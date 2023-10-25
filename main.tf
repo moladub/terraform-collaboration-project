343ef17c4a88bc5f54e6abc21b6c8457762970c9
michaelO <michael_oladugba@bmc.com>
2023-10-25 13:26:55 -0500

    changed files

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
 provider "aws" {
-    region     = "${var.region}"
-    access_key = "${var.access_key}"
-    secret_key = "${var.secret_key}"
+    region     = "us-east-2"
 }
 
 resource "aws_instance" "web-server" {
